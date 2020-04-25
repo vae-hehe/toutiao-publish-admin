76f3d10da1977b64bed64d9fb7e7f1edac211287
呵呵 <1840494062@qq.com>
2020-04-25 11:28:09 +0800

    发布文章频道处理

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -7,31 +7,30 @@
         <!-- :to="{name:'home'}"  to='/'-->
         <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
         <el-breadcrumb-item>内容管理</el-breadcrumb-item>
-        <el-breadcrumb-item>内容管理</el-breadcrumb-item>
+        <el-breadcrumb-item>素材管理</el-breadcrumb-item>
         <el-breadcrumb-item>发布文章</el-breadcrumb-item>
       </el-breadcrumb>
       <!-- /面包屑导航 -->
     </div>
     <!-- 表单 -->
-    <el-form ref="form" :model="form" label-width="40px">
+    <el-form ref="form" :model="article" label-width="40px">
       <el-form-item label="标题">
-        <el-input v-model="form.name"></el-input>
+        <el-input v-model="article.title"></el-input>
       </el-form-item>
       <el-form-item label="内容">
-        <el-input type="textarea" v-model="form.desc"></el-input>
+        <el-input type="textarea" v-model="article.content"></el-input>
       </el-form-item>
       <el-form-item label="封面">
-        <el-radio-group v-model="form.resource">
-          <el-radio label="单图"></el-radio>
-          <el-radio label="三图"></el-radio>
-          <el-radio label="无图"></el-radio>
-          <el-radio label="自动"></el-radio>
+        <el-radio-group v-model="article.cover.type">
+          <el-radio :label="1">单图</el-radio>
+          <el-radio :label="3">三图</el-radio>
+          <el-radio :label="0">无图</el-radio>
+          <el-radio :label="-1">自动</el-radio>
         </el-radio-group>
       </el-form-item>
       <el-form-item label="频道">
-        <el-select v-model="form.region" placeholder="请选择活动区域">
-          <el-option label="区域一" value="shanghai"></el-option>
-          <el-option label="区域二" value="beijing"></el-option>
+        <el-select v-model="article.channel_id" placeholder="请选择频道">
+          <el-option v-for="(channel, index) in channels" :key="index" :label="channel.name" :value="channel.id"></el-option>
         </el-select>
       </el-form-item>
       <el-form-item>
@@ -44,31 +43,39 @@
 </template>
 
 <script>
+import { getArticleChannels } from '@/api/article'
 export default {
   name: 'PublishIndex',
   components: {},
   props: {},
   data () {
     return {
-      form: {
-        name: '',
-        region: '',
-        date1: '',
-        date2: '',
-        delivery: false,
-        type: [],
-        resource: '',
-        desc: ''
+      article: {
+        title: '',
+        content: '', // 文章内容
+        cover: {
+          type: 0, // 封面的类型 -1,0,1,3
+          images: [] // 封面图片的地址
+        },
+        channels: [],
+        channel_id: null
       }
     }
   },
   computed: {},
   watch: {},
-  created () {},
+  created () {
+    this.loadChannels()
+  },
   mounted () {},
   methods: {
     onSubmit () {
       console.log('submit!')
+    },
+    loadChannels () {
+      getArticleChannels().then(res => {
+        this.channels = res.data.data.channels
+      })
     }
   }
 }
