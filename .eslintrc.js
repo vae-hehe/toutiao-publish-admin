69798ac5895355c00fb53efb7f8c0ce3edaa740e
呵呵 <1840494062@qq.com>
2020-04-29 09:36:29 +0800

    修改ESLint文件,使 代码声明未使用时不报错

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,8 @@
+/**
+ * ESLint 代码格式规范校验规范的配置文件
+ * 注意: 该文件如果修改,必须重启才能生效
+ */
+
 module.exports = {
   root: true,
   env: {
@@ -10,8 +15,10 @@ module.exports = {
   parserOptions: {
     parser: 'babel-eslint'
   },
+  // 自定义代码校验规范
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-unused-vars': 'off' // 关闭这个规则验证 off 或者 0
   }
 }
