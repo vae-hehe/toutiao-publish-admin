d25c6715ccd3d933796d7a7fffa5b6c3b7b4d009
呵呵 <1840494062@qq.com>
2020-05-05 19:37:35 +0800

    更新首页

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,27 +1,6 @@
 <template>
-<div class="">
-  <p>
-    唯有南山忆往情，
-    爱只清明雨上悲，
-    许诺今生惟爱你，
-    嵩山独坐泪已垂
-    半城沦陷烟沙,
-    烟来花笑尽终消散.
-    粉色信笺忆南山,
-    渲染忧伤离别时。
-    清明雨上花满楼自，
-    静夜思叹庐州月。
-    浅唱寻雾不煽情，
-    惟爱许嵩夜会美。
-    许梦半城烟沙扬
-    嵩间亭房道两旁
-    最迷纤雨下城府
-    酷暑南山忆庐阳知
-    唯悟世人情
-    爱君古人风
-    许郊民爱厚
-    嵩高云日明
-  </p>
+<div class="home">
+  <p>哈哈哈哈哈</p>
 </div>
 </template>
 
@@ -41,4 +20,12 @@ export default {
 }
 </script>
 
-<style scoped lang="less"></style>
+<style scoped lang="less">
+.home {
+  width: 100%;
+  height: 100%;
+  background: url(./2.jpg) no-repeat;
+  background-size: cover;
+  color: rgb(248, 47, 255);
+}
+</style>
